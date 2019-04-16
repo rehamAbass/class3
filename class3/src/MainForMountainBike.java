e34e4090ff54d47a81c8aa08a7e3f60fa6da3e2c
yoavrodeh <yoav.rodeh@gmail.com>
2019-04-16 20:22:08 +0300

    d

--- class3/src/MainForMountainBike.java
+++ class3/src/MainForMountainBike.java
@@ -2,7 +2,7 @@ public class MainForMountainBike {
 	public static void main(String[] args) {
 		MountainBike m = new MountainBike();
 		m.speedUp(10);
-		m.seatHeight(3.2);
+		m.setSeatHeight(3.2);
 		System.out.println(m);
 		// Speed = 10, seat = 3.20
 	}
